efa71e7405d3871e9b28ba4a25e0f1015e492bc1
Markgundam <mark25gundam@hotmail.com>
2014-02-18 13:19:44 +0100

    Unity Project
    Added Unity Project files

---
+++ UnityProject/Assets/scripts/PaddleMov.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+	var leftmostonscreen:float;
+	var rightmostonscreen:float;
+	
+	leftmostonscreen = Camera.main.ScreenToWorldPoint(Vector3(0,0,0)).x;
+	rightmostonscreen = Camera.main.ScreenToWorldPoint(Vector3(Screen.width,0,0)).x;
+
+	Debug.Log(leftmostonscreen + ' ' + rightmostonscreen);
+
+	if (transform.position.x < leftmostonscreen)
+	{
+		transform.position.x = rightmostonscreen;
+	}
+	
+	if (transform.position.x > rightmostonscreen)
+	{
+		transform.position.x = leftmostonscreen;
+	}
+
+	transform.Translate(Vector3.right * 5 * Input.GetAxis("Horizontal") * Time.deltaTime);
+
+
+}
---
+++ UnityProject/Assets/scripts/SphereGen.js
@@ -0,0 +1,47 @@
+﻿#pragma strict
+var redsphere:Rigidbody2D;
+var greensphere:Rigidbody2D;
+var yellowsphere:Rigidbody2D;
+var bluesphere:Rigidbody2D;
+
+function generatesphere () 
+{
+
+var colornumber:int;
+colornumber = Random.Range(1,5);
+
+Debug.Log(colornumber);
+
+var xposition:float;
+xposition = Random.Range(-2.5f,2.5f);
+
+Debug.Log(xposition);
+
+	switch(colornumber){
+	
+		case 1: 
+			Instantiate(redsphere, Vector3(xposition,5,0),Quaternion.identity);
+			break;
+		case 2: 
+			Instantiate(greensphere, Vector3(xposition,5,0),Quaternion.identity);
+			break;
+		case 3: 
+			Instantiate(yellowsphere, Vector3(xposition,5,0),Quaternion.identity); 
+			break;
+		case 4: 
+			Instantiate(bluesphere, Vector3(xposition,5,0),Quaternion.identity); 
+			break;
+		
+	}
+
+}
+
+function Start () {
+
+	InvokeRepeating("generatesphere",1.0,1.0);
+
+}
+
+function Update () {
+
+}
